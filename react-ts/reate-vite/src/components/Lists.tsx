1a1785eeb15808e58042e09a52cc7696aa4958e5
john <johnthomas222000@gmail.com>
2023-07-24 17:48:07 +0530

    final commit

--- react-ts/reate-vite/src/components/Lists.tsx
+++ react-ts/reate-vite/src/components/Lists.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from "react";
 
 interface ListProps<T> {
-  items: T[];
-  render: (item: T) => ReactNode;
+  items: string[];
+  render: (item: T | string) => ReactNode;
 }
 
 const Lists = <T,>({ items, render }: ListProps<T>) => {
